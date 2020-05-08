a45a7e730b31df6e1d24b2ecf48c83fd7142b14c
phil.xu <xupengfei2010@gmail.com>
2020-05-08 20:40:07 +0800

    cdn cache

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
-app.use(express.static('dist'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+const port = 3000;
+// 设置跨域访问
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
+app.use(express.static('dist'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- vue.config.js
+++ vue.config.js
@@ -13,12 +13,16 @@ module.exports = {
         {
           // To match cross-origin requests, use a RegExp that matches
           // the start of the origin:
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          urlPattern: /^http.*\.(?:png|jpg|jpeg|svg)$/,
           handler: 'staleWhileRevalidate',
           options: {
+            // cacheName: 'image',
+            // expiration: {
+            //   maxAgeSeconds: 10,
+            // },
             // Configure which responses are considered cacheable.
             cacheableResponse: {
-              statuses: [200],
+              statuses: [0, 200],
             },
           },
         },
