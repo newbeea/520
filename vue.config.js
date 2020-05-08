fd0a4b334e16922d773c77cd5de643a6112c5de1
phil.xu <xupengfei2010@gmail.com>
2020-05-08 19:22:28 +0800

    pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '//cdn520.malianghang.com/' : '/',
+  css: {
+    sourceMap: true,
+  },
+  pwa: {
+    workboxOptions: {
+      importWorkboxFrom: 'local',
+      skipWaiting: true,
+      clientsClaim: true,
+      exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+      runtimeCaching: [
+        {
+          // To match cross-origin requests, use a RegExp that matches
+          // the start of the origin:
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          handler: 'staleWhileRevalidate',
+          options: {
+            // Configure which responses are considered cacheable.
+            cacheableResponse: {
+              statuses: [200],
+            },
+          },
+        },
+      ],
+    },
+  },
 };
