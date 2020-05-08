ec6bb873dab9875183406684b42bd2651853dfa2
phil.xu <xupengfei2010@gmail.com>
2020-05-08 14:40:48 +0800

    typo

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV === 'production') {
   register('/service-worker.js', {
     ready() {
       console.log(
-        'App is being served from cache by a service worker.\n' +
-          'For more details, visit https://goo.gl/AFskqB'
+        'App is being served from cache by a service worker.\n'
+        + 'For more details, visit https://goo.gl/AFskqB',
       );
     },
     registered() {
